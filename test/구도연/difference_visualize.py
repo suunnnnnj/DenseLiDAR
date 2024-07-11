a3b90e5c1727628306267a10d9a824d37aee8844
9-coding <9.coding.lab@gmail.com>
2024-07-11 13:47:57 +0900

    FEAT: difference_visualize

---
+++ test/구도연/difference_visualize.py
@@ -0,0 +1,58 @@
+"""
+사용법
+command:
+    - python test.py --loadmodel depth_completion_KITTI.tar
+    - (test.py 사용법과 같음.)
+
+input:
+    - kitti_dataset
+
+how_to_use:
+    - DeepLiDAR project의 test.py에 difference_visualize 함수 붙여넣기
+    - test function의 pred1 = predC * predMaskC + predN * predMaskN 아래에서 함수 호출하기
+    - 함수 호출: difference_visualize(pred1, predN, predMaskN)
+    - 실행
+
+result:
+    - DeepLiDAR DCU의 Normal Pathway의 결과(depth, attention map)와 최종 출력을 시각화하여 비교할 수 있음.
+"""
+
+import cv2
+import matplotlib.pyplot as plt
+
+def difference_visualize(pred1, predN, predMaskN):
+    # pred1:     final dense depth
+    # predN:     normal pathway depth
+    # predMaskN: normal pathway attention map
+
+    # 1: normal_pathway - result
+    normalD_pred = cv2.subtract(predN.cpu().numpy().squeeze(), pred1.cpu().numpy().squeeze())
+    normalA_pred = cv2.subtract(predMaskN.cpu().numpy().squeeze(), pred1.cpu().numpy().squeeze())
+
+    fig = plt.figure(figsize=(10, 10))
+    fig.suptitle("Difference between Normal Pathway and Final Dense Depth")
+
+    plt.subplot(2, 2, 1)
+    plt.title(('Normal Pathway Depth - Final Dense Depth'))
+    plt.imshow(normalD_pred, 'gray')
+
+    plt.subplot(2, 2, 2)
+    plt.title(('Normal Pathway Attention Map - Final Dense Depth'))
+    plt.imshow(normalA_pred, 'gray')
+
+
+    # 2: result - normal_pathway
+    pred_normalD = cv2.subtract(pred1.cpu().numpy().squeeze(), predN.cpu().numpy().squeeze())
+    pred_normalA = cv2.subtract(pred1.cpu().numpy().squeeze(), predMaskN.cpu().numpy().squeeze())
+
+    plt.subplot(2, 2, 3)
+    plt.title(('Final Dense Depth - Normal Pathway Depth'))
+    plt.imshow(pred_normalD, 'gray')
+
+    plt.subplot(2, 2, 4)
+    plt.title(('Final Dense Depth - Normal Pathway Attention Map'))
+    plt.imshow(pred_normalA, 'gray')
+
+    plt.show()
+
+# difference_visualize(pred1, predN, predMaskN)
