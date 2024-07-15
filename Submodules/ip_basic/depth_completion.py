989f71c87c98ee045939722c75af42faf53928ff
Kim Dahye <106726878+dahye411@users.noreply.github.com>
2024-07-15 13:35:16 +0900

    FEAT: change extrapolate and blur_type option

--- Submodules/ip_basic/depth_completion.py
+++ Submodules/ip_basic/depth_completion.py
@@ -21,24 +21,26 @@ def ip_basic(sparse_depth_path):
     input_depth_dir = os.path.dirname(sparse_depth_path)
     data_split = 'val'
 
+    # Test set
+    # input_depth_dir = os.path.dirname(sparse_depth_path)
+    # data_split = 'test'
+    
     fill_type = 'fast'
-    extrapolate = True
-    blur_type = 'gaussian'
+    extrapolate = False
+    blur_type = 'bilateral'
 
+    # Save output to disk
     save_output = True
 
     ##############################
     # Processing
     ##############################
     if save_output:
-        show_process = False
         save_depth_maps = True
     else:
-        if fill_type == 'fast':
-            raise ValueError('"fast" fill does not support show_process')
-        show_process = True
         save_depth_maps = False
 
+    # Create output folder
     this_file_path = os.path.dirname(os.path.realpath(__file__))
     outputs_dir = os.path.join(this_file_path, 'outputs')
     os.makedirs(outputs_dir, exist_ok=True)
@@ -63,26 +65,25 @@ def ip_basic(sparse_depth_path):
             raise FileExistsError('Already exists!')
         print('Output dir:', output_depth_dir)
 
-    depth_image_path = sparse_depth_path
-
     # Load depth projections from uint16 image
+    depth_image_path = sparse_depth_path
     depth_image = cv2.imread(depth_image_path, cv2.IMREAD_ANYDEPTH)
     projected_depths = np.float32(depth_image / 256.0)
 
     # Fill in
-    start_fill_time = time.time()
     if fill_type == 'fast':
         final_depths = depth_map_utils.fill_in_fast(projected_depths, extrapolate=extrapolate, blur_type=blur_type)
     elif fill_type == 'multiscale':
         final_depths, process_dict = depth_map_utils.fill_in_multiscale(
-            projected_depths, extrapolate=extrapolate, blur_type=blur_type, show_process=show_process)
+            projected_depths, extrapolate=extrapolate, blur_type=blur_type)
     else:
         raise ValueError('Invalid fill_type {}'.format(fill_type))
-    end_fill_time = time.time()
 
     # Save depth images to disk
     if save_depth_maps:
         depth_image_file_name = os.path.basename(depth_image_path)
+
+        # Save depth map to a uint16 png (same format as disparity maps)
         file_path = os.path.join(output_depth_dir, depth_image_file_name)
         with open(file_path, 'wb') as f:
             depth_image = (final_depths * 256).astype(np.uint16)
