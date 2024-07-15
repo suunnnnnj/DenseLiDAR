c5ac93229e35e6711882ccba40e8911a129b0b88
jhcho53 <jjh000503@gachon.ac.kr>
2024-07-15 13:49:21 +0900

    FIX: fixed input data

--- Submodules/DCU/submodels/total_loss.py
+++ Submodules/DCU/submodels/total_loss.py
@@ -2,11 +2,8 @@ import torch
 from L1_structural_loss import l_structural
 from L2_depth_loss import L2_depth_loss
 
-def total_loss(pseudo_gt_map, gt, pseudo_depth_map, dense_depth_residual):
-    Dense_depth = pseudo_depth_map + dense_depth_residual
-    structural_loss = l_structural(pseudo_gt_map, Dense_depth)
-    depth_loss = L2_depth_loss(gt, pseudo_depth_map, dense_depth_residual)
+def total_loss(pseudo_gt_map, gt, dense_pseudo_depth):
+    structural_loss = l_structural(pseudo_gt_map, dense_pseudo_depth)
+    depth_loss = L2_depth_loss(gt, pseudo_depth_map, dense_pseudo_depth)
     return structural_loss + depth_loss
 
-loss = total_loss(D, D_pred, gt, pseudo_depth_map, dense_depth_residual)
-print(f"Total loss: {loss.item()}")
