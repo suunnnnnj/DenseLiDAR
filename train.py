cfb32469c29ae8095274b76295f988bf5a01be12
jhcho53 <jjh000503@gachon.ac.kr>
2024-07-16 09:13:23 +0900

    FEAT: update tqdm log

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from torch import optim
 from torch.nn import Module
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
+from tqdm import tqdm
 
 from Submodules.DCU.submodels.depthCompletionNew_blockN import depthCompletionNew_blockN, maskFt
 from Submodules.DCU.submodels.total_loss import total_loss
@@ -20,7 +21,7 @@ from model import DenseLiDAR
 def train(model, train_loader, optimizer, epoch, device):
     model.train()
     running_loss = 0.0
-    for i, data in enumerate(train_loader):
+    for i, data in enumerate(tqdm(train_loader, desc=f"Epoch {epoch + 1} [Training]")):
         annotated_image = data['annotated_image'].to(device)
         velodyne_image = data['velodyne_image'].to(device)
         raw_image = data['raw_image'].to(device)
@@ -32,7 +33,6 @@ def train(model, train_loader, optimizer, epoch, device):
         # 3채널 이미지를 그레이스케일로 변환
         targets_np_gray = cv2.cvtColor(targets_np[0].transpose(1, 2, 0), cv2.COLOR_RGB2GRAY)
         # np.float32로 변환
-        print("train pseudo gt map")
         pseudo_gt_map = ip_basic(np.float32(targets_np_gray / 256.0))
 
         optimizer.zero_grad()
@@ -40,23 +40,20 @@ def train(model, train_loader, optimizer, epoch, device):
         dense_pseudo_depth, pseudo_depth_map = model(raw_image, velodyne_image, device) 
         dense_pseudo_depth = dense_pseudo_depth.to(device)  # (B, H, W) -> (B, 1, H, W)
         dense_target = pseudo_gt_map.clone().detach().to(device)  # GPU로 이동
-        print(f"VType: {dense_pseudo_depth.dtype}, VShape: {dense_pseudo_depth.shape}")
-        print(f"VType: {dense_target.dtype}, VShape: {dense_target.shape}")
 
         loss = total_loss(dense_target, targets, dense_pseudo_depth)
         loss.backward()
         optimizer.step()
 
         running_loss += loss.item()
-    print(f"Epoch {epoch + 1} training loss: {running_loss / len(train_loader):.4f}")
-    
 
+    print(f"Epoch {epoch + 1} training loss: {running_loss / len(train_loader):.4f}")
 
 def validate(model, val_loader, epoch, device):
     model.eval()
     val_loss = 0.0
     with torch.no_grad():
-        for i, data in enumerate(val_loader):
+        for i, data in enumerate(tqdm(val_loader, desc=f"Epoch {epoch + 1} [Validation]")):
             annotated_image = data['annotated_image'].to(device)
             velodyne_image = data['velodyne_image'].to(device)
             raw_image = data['raw_image'].to(device)
@@ -67,9 +64,7 @@ def validate(model, val_loader, epoch, device):
             # 3채널 이미지를 그레이스케일로 변환
             targets_np_gray = cv2.cvtColor(targets_np[0].transpose(1, 2, 0), cv2.COLOR_RGB2GRAY)
             # np.float32로 변환
-            print("val pseudo gt map")
             pseudo_gt_map = ip_basic(np.float32(targets_np_gray / 256.0))
-            print("val pseudo depth map")
             pseudo_depth_map = ip_basic(velodyne_image.cpu().numpy().squeeze())
             
             dense_pseudo_depth = model(raw_image, velodyne_image, device)
