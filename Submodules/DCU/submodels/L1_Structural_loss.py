76c62ca76827ff7fc59aef28f804f0182db467de
Kim Dahye <106726878+dahye411@users.noreply.github.com>
2024-07-17 09:52:35 +0900

    FEAT: modify SSIM loss

--- Submodules/DCU/submodels/L1_Structural_loss.py
+++ Submodules/DCU/submodels/L1_Structural_loss.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn.functional as F
+from torch.autograd import Variable
 import numpy as np
+from math import ex
 from torchvision.transforms.functional import rgb_to_grayscale
 from torchvision import transforms
 
-#L1 Structural Loss
 def gradient_x(img):
     if img.dim() == 4:
         img = F.pad(img, (0, 0, 1, 0), mode='replicate')
@@ -13,13 +14,12 @@ def gradient_x(img):
         raise ValueError("Expected 4D tensor as input for gradient_x")
     return gx
 
-# HAVE SOME PROBLEM!!!! 
 def gradient_y(img):
     if img.dim() == 4:
         img = F.pad(img, (1, 0, 0, 0), mode='replicate')
         gy = img[:, :, :, :-1] - img[:, :, :, 1:]
     else:
-        raise ValueError("Expected 4D tensor as input for gradient_x")
+        raise ValueError("Expected 4D tensor as input for gradient_y")
     return gy
 
 def l_grad(D, D_pred):
@@ -30,23 +30,72 @@ def l_grad(D, D_pred):
     grad_loss = torch.mean(torch.abs(grad_x_true - grad_x_pred) + torch.abs(grad_y_true - grad_y_pred))
     return grad_loss
 
-def ssim(img1, img2):
-    C1 = 0.01 ** 2
-    C2 = 0.03 ** 2
+def gaussian(window_size, sigma):
+    gauss = torch.Tensor([exp(-(x - window_size//2)**2/float(2*sigma**2)) for x in range(window_size)])
+    return gauss/gauss.sum()
 
-    mu1 = F.avg_pool2d(img1, 3, 1, 0)
-    mu2 = F.avg_pool2d(img2, 3, 1, 0)
+def create_window(window_size, channel):
+    _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
+    _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
+    window = Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
+    return window
 
-    mu1_sq = mu1 ** 2
-    mu2_sq = mu2 ** 2
-    mu1_mu2 = mu1 * mu2
+def _ssim(img1, img2, window, window_size, channel, size_average = True):
+    mu1 = F.conv2d(img1, window, padding = window_size//2, groups = channel)
+    mu2 = F.conv2d(img2, window, padding = window_size//2, groups = channel)
 
-    sigma1_sq = F.avg_pool2d(img1 * img1, 3, 1, 0) - mu1_sq
-    sigma2_sq = F.avg_pool2d(img2 * img2, 3, 1, 0) - mu2_sq
-    sigma12 = F.avg_pool2d(img1 * img2, 3, 1, 0) - mu1_mu2
+    mu1_sq = mu1.pow(2)
+    mu2_sq = mu2.pow(2)
+    mu1_mu2 = mu1*mu2
 
-    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2))
-    return ssim_map.mean()
+    sigma1_sq = F.conv2d(img1*img1, window, padding = window_size//2, groups = channel) - mu1_sq
+    sigma2_sq = F.conv2d(img2*img2, window, padding = window_size//2, groups = channel) - mu2_sq
+    sigma12 = F.conv2d(img1*img2, window, padding = window_size//2, groups = channel) - mu1_mu2
+
+    C1 = 0.01**2
+    C2 = 0.03**2
+
+    ssim_map = ((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*(sigma1_sq + sigma2_sq + C2))
+
+    if size_average:
+        return ssim_map.mean()
+    else:
+        return ssim_map.mean(1).mean(1).mean(1)
+
+class SSIM(torch.nn.Module):
+    def __init__(self, window_size = 11, size_average = True):
+        super(SSIM, self).__init__()
+        self.window_size = window_size
+        self.size_average = size_average
+        self.channel = 1
+        self.window = create_window(window_size, self.channel)
+
+    def forward(self, img1, img2):
+        (_, channel, _, _) = img1.size()
+
+        if channel == self.channel and self.window.data.type() == img1.data.type():
+            window = self.window
+        else:
+            window = create_window(self.window_size, channel)
+            
+            if img1.is_cuda:
+                window = window.cuda(img1.get_device())
+            window = window.type_as(img1)
+            
+            self.window = window
+            self.channel = channel
+
+        return _ssim(img1, img2, window, self.window_size, channel, self.size_average)
+
+def ssim(img1, img2, window_size = 11, size_average = True):
+    (_, channel, _, _) = img1.size()
+    window = create_window(window_size, channel)
+    
+    if img1.is_cuda:
+        window = window.cuda(img1.get_device())
+    window = window.type_as(img1)
+    
+    return _ssim(img1, img2, window, window_size, channel, size_average)
 
 def l_ssim(D, D_pred):
     ssim_loss = 1 - ssim(D, D_pred)
