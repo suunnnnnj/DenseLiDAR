32496beb7d1eb348f426e3fcce2022cfac76f6df
dahye411 <sere411@naver.com>
2024-07-18 09:21:17 +0900

    FEAT: fixed L2 depth loss

--- Submodules/loss/L2_depth_loss.py
+++ Submodules/loss/L2_depth_loss.py
@@ -5,7 +5,16 @@ def L2_depth_loss(gt, dense_depth):
         gt = torch.tensor(gt)
     if not isinstance(dense_depth, torch.Tensor):
         dense_depth = torch.tensor(dense_depth)
+    
+    if gt.shape[1] > 1:
+        gt = torch.mean(gt, dim=1, keepdim=True)
 
-    n = gt.size(0)
-    loss = torch.sum((gt - dense_depth) ** 2) / n
+    mask = gt > 0
+
+    gt_masked = gt[mask]
+    dense_depth_masked = dense_depth[mask]
+
+    n = gt_masked.size(0)
+
+    loss = torch.sum((gt_masked - dense_depth_masked) ** 2) / n
     return loss
