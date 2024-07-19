745118caa9ee8f746b07a070101803545ddd2d45
9-coding <9.coding.lab@gmail.com>
2024-07-19 10:23:28 +0900

    FIX: connnect set_image function

--- Submodules/utils/visualization.py
+++ Submodules/utils/visualization.py
@@ -4,25 +4,25 @@ from torchvision.transforms import transforms
 
 def set_image(img):
     if isinstance(img, PIL.Image.Image):
-        pass
-    elif img.ndim == 4:
+        return img
+
+    if img.ndim == 4:
         if img.shape[1] == 3:
             grayscale_transform = transforms.Grayscale(num_output_channels=1)
             img = grayscale_transform(img)
-        img = img.squeeze().squeeze()
-        if img.type != 'cpu':
-            img = img.cpu()
+        img = img.squeeze().squeeze().cpu().numpy()
+
     return img
 
 def visualize_1(title, img):
-    set_image(img)
+    img = set_image(img)
     plt.title(title)
     plt.imshow(img, 'gray')
     plt.show()
 
 def visualize_2(title, title1, title2, img1, img2):
-    set_image(img1)
-    set_image(img2)
+    img1 = set_image(img1)
+    img2 = set_image(img2)
 
     plt.suptitle(title)
     plt.subplot(2,1,1)
