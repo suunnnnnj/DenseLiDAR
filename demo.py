abc2a51f3e7908e6d8bed3087bce559463eae08e
dahye411 <sere411@naver.com>
2024-07-25 15:50:01 +0900

    REFACTOR: demo.py

--- demo.py
+++ demo.py
@@ -7,7 +7,6 @@ import torch.nn.parallel
 import torch.utils.data
 import torchvision.transforms as transforms
 from torch.autograd import Variable
-import torch.nn.functional as F
 import skimage
 import skimage.io
 import skimage.transform
@@ -18,12 +17,9 @@ from PIL import Image
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 parser = argparse.ArgumentParser(description='deepCompletion')
-parser.add_argument('--loadmodel', default='',
-                    help='load model')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='enables CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
+parser.add_argument('--loadmodel', default='', help='load model')
+parser.add_argument('--no-cuda', action='store_true', default=False, help='enables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -35,10 +31,10 @@ model = DenseLiDAR(1)
 # model = nn.DataParallel(model, device_ids=[0])
 model.cuda()
 
-modelpath = os.path.join(ROOT_DIR, args.loadmodel)
+model_path = os.path.join(ROOT_DIR, args.loadmodel)
 
-if modelpath is not None:
-    checkpoint = torch.load(modelpath, map_location=torch.device('cpu'))
+if model_path is not None:
+    checkpoint = torch.load(model_path, map_location=torch.device('cpu'))
     print("Checkpoint keys:", checkpoint.keys())  # Check the structure of the checkpoint
 
     # Check if 'model_state_dict' directly exists in checkpoint or inside another key
@@ -53,78 +49,75 @@ if modelpath is not None:
     # Load the state_dict into the model
     model.load_state_dict(state_dict, strict=False)  # Set strict=False to ignore unmatched keys
 
-    print(f"Loaded pretrained model weights from {modelpath}.")
+    print(f"Loaded pretrained model weights from {model_path}.")
 
 
-def test(imgL,sparse,pseudo_depth):
+def test(ori_image,sparse,pseudo_depth):
         model.eval()
 
         if args.cuda:
-           imgL = torch.FloatTensor(imgL).cuda()
+           ori_image = torch.FloatTensor(ori_image).cuda()
            sparse = torch.FloatTensor(sparse).cuda()
            pseudo_depth = torch.FloatTensor(pseudo_depth).cuda()
 
-        imgL= Variable(imgL)
+        ori_image= Variable(ori_image)
         sparse = Variable(sparse)
         pseudo_depth = Variable(pseudo_depth)
 
         device = "cuda"
         with torch.no_grad():
-            dense_depth = model(imgL, sparse, pseudo_depth, device)
+            dense_depth = model(ori_image, sparse, pseudo_depth, device)
 
         dense_depth = torch.squeeze(dense_depth)
 
         return dense_depth.data.cpu().numpy()
 
-def scale_crop():
+
+def get_transform():
     t_list = [
         transforms.ToTensor()
     ]
 
     return transforms.Compose(t_list)
 
-def get_transform():
-    return scale_crop()
 
 def main():
    processed = get_transform()
 
-   pseudo_depth_fold = 'demo_pseudo_depth.png'
-   left_fold = 'demo_image.png'
-   lidar2_raw ='demo_velodyne.png'
+   image_path = 'demo_image.png'
+   sparse_path ='demo_velodyne.png'
+   pseudo_depth_path = 'demo_pseudo_depth.png'
    
-   imgL_o = skimage.io.imread(left_fold)
-   imgL_o = np.reshape(imgL_o, [imgL_o.shape[0], imgL_o.shape[1],3])
-   imgL = processed(imgL_o).numpy()
-   imgL = np.reshape(imgL, [1, 3, imgL_o.shape[0], imgL_o.shape[1]])
+   ori_image = skimage.io.imread(image_path)
+   ori_image = np.reshape(ori_image, [ori_image.shape[0], ori_image.shape[1], 3])
+   image = processed(ori_image).numpy()
+   image = np.reshape(image, [1, 3, ori_image.shape[0], ori_image.shape[1]])
 
-   p_depth = skimage.io.imread(pseudo_depth_fold).astype(np.float32)
-   p_depth = p_depth * 1.0 / 256.0
-   p_depth = np.reshape(p_depth, [imgL_o.shape[0], imgL_o.shape[1], 1])
-   p_depth = processed(p_depth).numpy()
-   p_depth = np.reshape(p_depth, [1, 1, imgL_o.shape[0], imgL_o.shape[1]])
-
-   sparse = skimage.io.imread(lidar2_raw).astype(np.float32)
-   sparse = sparse *1.0 / 256.0
-   sparse = np.reshape(sparse, [imgL_o.shape[0], imgL_o.shape[1], 1])
+   sparse = skimage.io.imread(sparse_path).astype(np.float32)
+   sparse = sparse * 1.0 / 256.0
+   sparse = np.reshape(sparse, [ori_image.shape[0], ori_image.shape[1], 1])
    sparse = processed(sparse).numpy()
-   sparse = np.reshape(sparse, [1, 1, imgL_o.shape[0], imgL_o.shape[1]])
+   sparse = np.reshape(sparse, [1, 1, ori_image.shape[0], ori_image.shape[1]])
 
-   output1 = "dense_depth_output.png"
+   pseudo_depth = skimage.io.imread(pseudo_depth_path).astype(np.float32)
+   pseudo_depth = pseudo_depth * 1.0 / 256.0
+   pseudo_depth = np.reshape(pseudo_depth, [ori_image.shape[0], ori_image.shape[1], 1])
+   pseudo_depth = processed(pseudo_depth).numpy()
+   pseudo_depth = np.reshape(pseudo_depth, [1, 1, ori_image.shape[0], ori_image.shape[1]])
 
-   pred = test(imgL, sparse, p_depth)
-   pred = np.where(pred <= 0.0, 0.9, pred)
+   output_path = "dense_depth_output.png"
 
-   pred_show = pred * 256.0
-   pred_show = pred_show.astype('uint16')
-   res_buffer = pred_show.tobytes()
-   img = Image.new("I",pred_show.T.shape)
-   img.frombytes(res_buffer,'raw',"I;16")
-   img.save(output1)
+   final_dense_depth = test(image, sparse, pseudo_depth)
+   final_dense_depth = np.where(final_dense_depth <= 0.0, 0.9, final_dense_depth)
+
+   final_dense_depth = final_dense_depth * 256.0
+   final_dense_depth = final_dense_depth.astype('uint16')
+
+   res_buffer = final_dense_depth.tobytes()
+
+   output_image = Image.new("I",final_dense_depth.T.shape)
+   output_image.frombytes(res_buffer,'raw',"I;16")
+   output_image.save(output_path)
 
 if __name__ == '__main__':
    main()
-
-
-
-
