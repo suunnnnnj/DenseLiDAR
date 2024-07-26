8251a61b73e725d03b9fd429b1a66237d314ba93
dahye411 <sere411@naver.com>
2024-07-26 15:48:55 +0900

    REFACTOR: refactoring train.py and demo.py

--- demo.py
+++ demo.py
@@ -1,97 +1,75 @@
-#argument : model path ,demo image path, demo velodyne path, demo pseudo depth map path
-#Ex : python3 demo.py --model_path checkpoint/epoch-5_loss-4.412.tar --left_image_path demo_image.png --sparse_depth_path demo_velodyne.png --pseudo_depth_map_path demo_pseudo_depth.png 
+"""
+Argument : model path, demo image path, demo sparse LiDAR data path, demo pseudo depth map path, output path
+Example : python demo.py --model_path checkpoint/epoch-5_loss-3.273.tar --image_path demo/demo_image.png --sparse_path demo/demo_velodyne.png --pseudo_depth_map_path demo/demo_pseudo_depth.png --output_path demo/dense_depth_output.png
+"""
 
 import torch
-from torch.nn import Module
-import cv2
-import matplotlib.pyplot as plt
-import argparse
 import torchvision.transforms as transforms
+import cv2
+import argparse
 import numpy as np
-from model import DenseLiDAR  # 필요한 모듈을 임포트하세요
-
-def visualize_tensor(tensor, title="Tensor Visualization", num_channels_to_display=3):
-    tensor = tensor.detach().cpu().numpy()
-    
-    if tensor.ndim == 4:  # Batch, Channels, Height, Width
-        tensor = tensor[0]  # 배치의 첫 번째 샘플 선택
-    
-    num_channels = tensor.shape[0]
-    
-    if num_channels == 1:  # 단일 채널 이미지
-        tensor = tensor[0]  # 채널 차원 제거
-        plt.imshow(tensor, cmap='gray')
-        plt.title(f"{title} (channel 1)")
-        plt.axis('off')
-        plt.show()
-    elif num_channels == 3:  # RGB 이미지
-        tensor = np.transpose(tensor, (1, 2, 0))  # 차원을 Height, Width, Channels로 재배치
-        plt.imshow(tensor)
-        plt.title(title)
-        plt.axis('off')
-        plt.show()
-    else:
-        # 첫 몇 개 채널을 별도 이미지로 표시
-        fig, axes = plt.subplots(1, num_channels_to_display, figsize=(15, 5))
-        for i in range(min(num_channels, num_channels_to_display)):
-            axes[i].imshow(tensor[i], cmap='gray')
-            axes[i].set_title(f"{title} (channel {i + 1})")
-            axes[i].axis('off')
-        plt.show()
+from model import DenseLiDAR
+# from Submodules.utils.visualization import *
 
 def load_image(image_path):
     image = cv2.imread(image_path, cv2.IMREAD_COLOR)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     transform = transforms.ToTensor()
     image_tensor = transform(image).unsqueeze(0).float()
+
     return image_tensor.cuda()
 
-def load_depth_map(depth_map_path):
-    depth_map = cv2.imread(depth_map_path, cv2.IMREAD_ANYDEPTH)
-    depth = depth_map.astype(np.float32) / 256.0
-    depth_tensor = torch.tensor(depth).unsqueeze(0).unsqueeze(0).float()
-    return depth_tensor.cuda()
+
+def load_pseudo_depth_map(pseudo_depth_map_path):
+    pseudo_depth_map = cv2.imread(pseudo_depth_map_path, cv2.IMREAD_ANYDEPTH)
+    pseudo_depth_map = pseudo_depth_map.astype(np.float32) / 256.0
+    pseudo_depth_map_tensor = torch.tensor(pseudo_depth_map).unsqueeze(0).unsqueeze(0).float()
+
+    return pseudo_depth_map_tensor.cuda()
+
 
 def remove_module_prefix(state_dict):
-    # 'module.' 접두사를 제거하여 state_dict 키를 수정
     new_state_dict = {}
     for k, v in state_dict.items():
-        name = k[7:] if k.startswith('module.') else k  # remove `module.`
+        name = k[7:] if k.startswith('module.') else k
         new_state_dict[name] = v
+
     return new_state_dict
 
-def main(model_path, left_image_path, sparse_depth_path, pseudo_depth_map_path):
-    # 이미지와 깊이 맵 로드
-    left_image = load_image(left_image_path)
-    sparse_depth = load_depth_map(sparse_depth_path)
-    pseudo_depth_map = load_depth_map(pseudo_depth_map_path)
 
-    # 모델 초기화
-    model = DenseLiDAR(bs=1).cuda()  # bs 매개변수 설정
+def save_depth_map(dense_depth, output_path):
+    dense_depth = dense_depth.squeeze().cpu().numpy()
+    dense_depth = (dense_depth * 256).astype(np.uint16)
+    cv2.imwrite(output_path, dense_depth)
+
+
+def main(model_path, image_path, sparse_path, pseudo_depth_map_path, output_path):
+    image = load_image(image_path)
+    sparse = load_pseudo_depth_map(sparse_path)
+    pseudo_depth_map = load_pseudo_depth_map(pseudo_depth_map_path)
+
+    model = DenseLiDAR(bs=1).cuda()
     
-    # 사전 학습된 모델 로드
     checkpoint = torch.load(model_path)
     state_dict = remove_module_prefix(checkpoint['model_state_dict'])
     model.load_state_dict(state_dict)
     model.eval()
     
-    # Forward pass
     with torch.no_grad():
-        final_dense_depth = model(left_image, sparse_depth, pseudo_depth_map, 'cuda')
+        final_dense_depth = model(image, sparse, pseudo_depth_map, 'cuda')
 
-    # 결과 시각화
-    visualize_tensor(left_image, title="Left Image")
-    visualize_tensor(sparse_depth, title="Sparse Depth")
-    visualize_tensor(pseudo_depth_map, title="Pseudo Depth Map")
-    visualize_tensor(final_dense_depth, title="Final Dense Depth Map")
     final_dense_depth = final_dense_depth.cpu()
-    print(np.unique(final_dense_depth))
+    save_depth_map(final_dense_depth, output_path)
+    print(f"Final dense depth map saved to {output_path}")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='DenseLiDAR Inference Demo')
     parser.add_argument('--model_path', type=str, required=True, help='Path to the pretrained model')
-    parser.add_argument('--left_image_path', type=str, required=True, help='Path to the left image')
-    parser.add_argument('--sparse_depth_path', type=str, required=True, help='Path to the sparse depth map')
+    parser.add_argument('--image_path', type=str, required=True, help='Path to the image')
+    parser.add_argument('--sparse_path', type=str, required=True, help='Path to the sparse LiDAR data')
     parser.add_argument('--pseudo_depth_map_path', type=str, required=True, help='Path to the pseudo depth map')
+    parser.add_argument('--output_path', type=str, required=True, help='Path to save the final dense depth map')
     args = parser.parse_args()
     
-    main(args.model_path, args.left_image_path, args.sparse_depth_path, args.pseudo_depth_map_path)
+    main(args.model_path, args.image_path, args.sparse_path, args.pseudo_depth_map_path, args.output_path)
--- train.py
+++ train.py
@@ -1,31 +1,27 @@
-from __future__ import print_function
 import argparse
 import os
 import torch
-import torch.nn as nn
-import torch.optim as optim
 import torch.utils.data
-from torch.autograd import Variable
 import torch.distributed as dist
-from torch.nn.parallel import DistributedDataParallel as DDP
-import numpy as np
+import torch.multiprocessing as mp
 import time
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.autograd import Variable
+from torch.optim import AdamW
 from dataloader import dataLoader as lsn
 from dataloader import trainLoader as DA
 from model import DenseLiDAR
 from submodules.loss.total_loss import total_loss
 from tqdm import tqdm
-import torch.multiprocessing as mp
-from torch.optim import AdamW
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-parser = argparse.ArgumentParser(description='deepCompletion')
-parser.add_argument('--datapath', default='', help='datapath')
+parser = argparse.ArgumentParser(description='depth completion')
+parser.add_argument('--data_path', default='', help='data path')
 parser.add_argument('--epochs', type=int, default=40, help='number of epochs to train')
 parser.add_argument('--checkpoint', type=int, default=5, help='number of epochs to make a checkpoint')
 parser.add_argument('--batch_size', type=int, default=1, help='batch size to train')
-parser.add_argument('--gpu_nums', type=int, default=1, help='number of GPUs to train')  # gpu_nums를 world_size로 사용
+parser.add_argument('--gpu_nums', type=int, default=1, help='number of GPUs to train')
 parser.add_argument('--loadmodel', default='', help='load model')
 parser.add_argument('--savemodel', default='my', help='save model')
 parser.add_argument('--no-cuda', action='store_true', default=False, help='enables CUDA training')
@@ -39,13 +35,16 @@ def setup(rank, world_size, master_addr, master_port):
     os.environ['MASTER_PORT'] = master_port
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
+
 def cleanup():
     dist.destroy_process_group()
+
 
 def main_worker(rank, world_size, args):
     batch_size = int(args.batch_size / args.gpu_nums)
     args.cuda = not args.no_cuda and torch.cuda.is_available()
     torch.manual_seed(args.seed)
+
     if args.cuda:
         torch.cuda.manual_seed(args.seed)
 
@@ -53,23 +52,23 @@ def main_worker(rank, world_size, args):
     
     torch.cuda.set_device(rank)
     
-    train_left_img, train_sparse, train_depth, train_pseudo, train_dense = lsn.dataloader(args.datapath, mode='train')
-    val_left_img, val_sparse, val_depth, val_pseudo, val_dense = lsn.dataloader(args.datapath, mode='val')
+    train_image, train_sparse, train_gt, train_pseudo_depth_map, train_pseudo_gt_map = lsn.dataloader(args.data_path, mode='train')
+    val_image, val_sparse, val_gt, val_pseudo_depth_map, val_pseudo_gt_map = lsn.dataloader(args.data_path, mode='val')
 
     train_sampler = torch.utils.data.distributed.DistributedSampler(
-        DA.myImageFloder(train_left_img, train_sparse, train_depth, train_pseudo, train_dense, True),
+        DA.myImageFloder(train_image, train_sparse, train_gt, train_pseudo_depth_map, train_pseudo_gt_map, True),
         num_replicas=world_size,
         rank=rank
     )
 
     val_sampler = torch.utils.data.distributed.DistributedSampler(
-        DA.myImageFloder(val_left_img, val_sparse, val_depth, val_pseudo, val_dense, True),
+        DA.myImageFloder(val_image, val_sparse, val_gt, val_pseudo_depth_map, val_pseudo_gt_map, True),
         num_replicas=world_size,
         rank=rank
     )
 
     TrainImgLoader = torch.utils.data.DataLoader(
-        DA.myImageFloder(train_left_img, train_sparse, train_depth, train_pseudo, train_dense, True),
+        DA.myImageFloder(train_image, train_sparse, train_gt, train_pseudo_depth_map, train_pseudo_gt_map, True),
         batch_size=batch_size,
         shuffle=False,
         num_workers=8,
@@ -79,7 +78,7 @@ def main_worker(rank, world_size, args):
     )
 
     ValImgLoader = torch.utils.data.DataLoader(
-        DA.myImageFloder(val_left_img, val_sparse, val_depth, val_pseudo, val_dense, True),
+        DA.myImageFloder(val_image, val_sparse, val_gt, val_pseudo_depth_map, val_pseudo_gt_map, True),
         batch_size=batch_size,
         shuffle=False,
         num_workers=8,
@@ -94,81 +93,108 @@ def main_worker(rank, world_size, args):
     optimizer = AdamW(model.parameters(), lr=0.001, weight_decay=1e-2)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs, eta_min=1e-7, last_epoch=-1)
 
+
     def save_model(model, optimizer, epoch, path):
         os.makedirs(os.path.dirname('checkpoint/'), exist_ok=True)
+
         if rank == 0:
             torch.save({
                 'epoch': epoch,
                 'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict()
             }, path)
+            
             print(f'Checkpoint saved at: {path}\n')
 
-    def train(inputl, gt1, sparse, pseudo, dense):
+
+    def train(image, gt, sparse, pseudo_depth_map, pseudo_gt_map):
         device = 'cuda'
         model.train()
-        inputl = Variable(torch.FloatTensor(inputl))
-        gt1 = Variable(torch.FloatTensor(gt1))
+
+        image = Variable(torch.FloatTensor(image))
+        gt = Variable(torch.FloatTensor(gt))
         sparse = Variable(torch.FloatTensor(sparse))
-        pseudo = Variable(torch.FloatTensor(pseudo))
-        dense = Variable(torch.FloatTensor(dense))
+        pseudo_depth_map = Variable(torch.FloatTensor(pseudo_depth_map))
+        pseudo_gt_map = Variable(torch.FloatTensor(pseudo_gt_map))
+
         if args.cuda:
-            inputl, gt1, sparse, pseudo, dense = inputl.cuda(), gt1.cuda(), sparse.cuda(), pseudo.cuda(), dense.cuda()
+            image, gt, sparse, pseudo_depth_map, pseudo_gt_map = image.cuda(), gt.cuda(), sparse.cuda(), pseudo_depth_map.cuda(), pseudo_gt_map.cuda()
         optimizer.zero_grad()
 
-        dense_depth = model(inputl, sparse, pseudo, device)
+        dense_depth = model(image, sparse, pseudo_depth_map, device)
 
-        t_loss, s_loss, d_loss = total_loss(dense, gt1, dense_depth)
+        t_loss, s_loss, d_loss = total_loss(pseudo_gt_map, gt, dense_depth)
         t_loss.backward()
         optimizer.step()
 
         return t_loss, s_loss, d_loss
 
-    def validate(inputl, gt1, sparse, pseudo, dense):
+
+    def validate(image, gt, sparse, pseudo_depth_map, pseudo_gt_map):
         device = 'cuda'
         model.eval()
-        with torch.no_grad():
-            inputl = Variable(torch.FloatTensor(inputl))
-            gt1 = Variable(torch.FloatTensor(gt1))
-            sparse = Variable(torch.FloatTensor(sparse))
-            pseudo = Variable(torch.FloatTensor(pseudo))
-            dense = Variable(torch.FloatTensor(dense))
-            if args.cuda:
-                inputl, gt1, sparse, pseudo, dense= inputl.cuda(), gt1.cuda(), sparse.cuda(), pseudo.cuda(), dense.cuda()
 
-            dense_depth = model(inputl, sparse, pseudo, device)
-            t_loss, s_loss, d_loss = total_loss(dense, gt1, dense_depth)
+        with torch.no_grad():
+            image = Variable(torch.FloatTensor(image))
+            gt = Variable(torch.FloatTensor(gt))
+            sparse = Variable(torch.FloatTensor(sparse))
+            pseudo_depth_map = Variable(torch.FloatTensor(pseudo_depth_map))
+            pseudo_gt_map = Variable(torch.FloatTensor(pseudo_gt_map))
+
+            if args.cuda:
+                image, gt, sparse, pseudo_depth_map, pseudo_gt_map = image.cuda(), gt.cuda(), sparse.cuda(), pseudo_depth_map.cuda(), pseudo_gt_map.cuda()
+
+            dense_depth = model(image, sparse, pseudo_depth_map, device)
+
+            t_loss, s_loss, d_loss = total_loss(pseudo_gt_map, gt, dense_depth)
 
         return t_loss, s_loss, d_loss
 
+
     torch.cuda.empty_cache()
+
     start_full_time = time.time()
 
     for epoch in range(1, args.epochs + 1):
         total_train_loss = 0
+        total_train_s_loss = 0
+        total_train_d_loss = 0
+
         total_val_loss = 0
+        total_val_s_loss = 0
+        total_val_d_loss =0
 
         TrainImgLoader.sampler.set_epoch(epoch)
 
         ## training ##
-        for batch_idx, (imgL_crop, input_crop1, sparse2, pseudo_crop, dense_crop) in tqdm(
-                enumerate(TrainImgLoader), total=len(TrainImgLoader), desc=f"Epoch {epoch}"):  # rawimage, gtlidar,rawlidar,pseudo_depth,gt_depth,param
+        for batch_idx, (image, gt, sparse, pseudo_depth_map, pseudo_gt_map) in tqdm(
+                enumerate(TrainImgLoader), total=len(TrainImgLoader), desc=f"Epoch {epoch}"):
 
-            loss, loss1, loss2 = train(imgL_crop, input_crop1, sparse2, pseudo_crop, dense_crop)
-            total_train_loss += loss
+            train_loss, train_s_loss, train_d_loss = train(image, gt, sparse, pseudo_depth_map, pseudo_gt_map)
+
+            total_train_loss += train_loss
+            total_train_s_loss += train_s_loss
+            total_train_d_loss += train_d_loss
 
         print('epoch %d total training loss = %.10f' % (epoch, total_train_loss / len(TrainImgLoader)))
-        print('epoch %d structural training loss = %.10f, depth training loss = %.10f' % (epoch, loss1, loss2))
+        print('epoch %d structural training loss = %.10f, depth training loss = %.10f' % (epoch, total_train_s_loss / len(TrainImgLoader), total_train_d_loss / len(TrainImgLoader)))
+        print()
 
         ## validation ##
-        for batch_idx, (imgL_crop, input_crop1, sparse2, pseudo_crop, dense_crop) in tqdm(
-                enumerate(ValImgLoader), total=len(ValImgLoader),
-                desc=f"Epoch {epoch}"):  # rawimage, gtlidar,rawlidar,pseudo_depth,gt_depth,param
+        for batch_idx, (image, gt, sparse, pseudo_depth_map, pseudo_gt_map) in tqdm(
+                enumerate(ValImgLoader), total=len(ValImgLoader), desc=f"Epoch {epoch}"):
 
-            loss, loss1, loss2 = validate(imgL_crop, input_crop1, sparse2, pseudo_crop, dense_crop)
-            total_val_loss += loss
+            val_loss, val_s_loss, val_d_loss = validate(image, gt, sparse, pseudo_depth_map, pseudo_gt_map)
+
+            total_val_loss += val_loss
+            total_val_s_loss += val_s_loss
+            total_val_d_loss += val_d_loss
 
         print('epoch %d total validation loss = %.10f' % (epoch, total_val_loss / len(ValImgLoader)))
+        print('epoch %d structural training loss = %.10f, depth training loss = %.10f' % (epoch, total_val_s_loss / len(ValImgLoader), total_val_d_loss / len(ValImgLoader)))
+        print()
+
+        scheduler.step()
 
         if epoch % args.checkpoint == 0 and rank == 0:
             save_path = f'checkpoint/epoch-{epoch}_loss-{total_val_loss / len(ValImgLoader):.3f}.tar'
@@ -180,7 +206,4 @@ def main_worker(rank, world_size, args):
 
 if __name__ == '__main__':
     world_size = args.gpu_nums
-    mp.spawn(main_worker,
-             args=(world_size, args),
-             nprocs=world_size,
-             join=True)
+    mp.spawn(main_worker, args=(world_size, args), nprocs=world_size, join=True)
