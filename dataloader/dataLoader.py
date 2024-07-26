46cd7046d0a721ad30588fd7b3a5e13ea499e80d
dahye411 <sere411@naver.com>
2024-07-26 16:39:50 +0900

    REFACTOR: refactoring trainLoader.py and dataLoader.py

--- dataloader/dataLoader.py
+++ dataloader/dataLoader.py
@@ -12,11 +12,13 @@ IMG_EXTENSIONS = [
 def is_image_file(filename):
     return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
 
+
 def get_fold_path(filepath, seq, fold):
     dir = os.path.join(filepath, seq, fold)
     path = [os.path.join(dir, img) for img in os.listdir(dir) if is_image_file(img)]
     path.sort()
     return path
+
 
 def get_file_path(filepath, seq):
     left_fold = 'proj_depth/image_02'
@@ -28,35 +30,38 @@ def get_file_path(filepath, seq):
     paths = np.append(left, right)
     return paths
 
+
 def dataloader(filepath, mode='train'):
     if mode not in ['train', 'val']:
         raise ValueError("mode should be either 'train' or 'val'")
 
     temp = filepath
-    filepath_ri = os.path.join(temp, f'kitti_raw/{mode}')
-    filepath_gtl = os.path.join(temp, f'data_depth_annotated/{mode}')
-    filepath_rl = os.path.join(temp, f'data_depth_velodyne/{mode}')
-    filepath_pd = os.path.join(temp, f'pseudo_depth_map/{mode}')
-    filepath_pg = os.path.join(temp, f'pseudo_gt_map/{mode}')
+    filepath_ri = os.path.join(temp, f'kitti_raw/{mode}')             # file path of raw image
+    filepath_gtl = os.path.join(temp, f'data_depth_annotated/{mode}') # file path of gt lidar
+    filepath_rl = os.path.join(temp, f'data_depth_velodyne/{mode}')   # file path of raw lidar
+    filepath_pd = os.path.join(temp, f'pseudo_depth_map/{mode}')      # file path of pseudo depth map
+    filepath_pg = os.path.join(temp, f'pseudo_gt_map/{mode}')         # file path of pseudo gt map
 
     seqs = [seq for seq in os.listdir(filepath_ri) if seq.find('sync') > -1]
     
     for seq in seqs:
-        images = get_file_path(filepath_ri, seq)    # raw image
-        lidars = get_file_path(filepath_rl, seq)    # raw lidar
-        gt = get_file_path(filepath_gtl, seq)       # ground truth lidar
-        pseudo = get_file_path(filepath_pd, seq)    # pseudo depth map
-        pseudo_gt = get_file_path(filepath_pg, seq) # pseudo GT map
+        image = get_file_path(filepath_ri, seq)            # raw image
+        gt_lidar = get_file_path(filepath_gtl, seq)        # ground truth lidar
+        raw_lidar = get_file_path(filepath_rl, seq)        # raw lidar
+        pseudo_depth_map = get_file_path(filepath_pd, seq) # pseudo depth map
+        pseudo_gt_map = get_file_path(filepath_pg, seq)    # pseudo gt map
 
-    print(mode)
-    print(f"\nimage: {len(images)} | lidar : {len(lidars)} | gt depth: {len(gt)} | pseudo depth: {len(pseudo)} | pseudo GT: {len(pseudo_gt)}")
+    print(f"[Number of {mode} files]")
+    print(f"image: {len(image)} | raw lidar: {len(raw_lidar)} | gt lidar: {len(gt_lidar)} | pseudo depth map: {len(pseudo_depth_map)} | pseudo gt map: {len(pseudo_gt_map)}\n")
 
     if mode == 'val':
         print("Data loading complete.\n")
 
-    return images, lidars, gt, pseudo, pseudo_gt
+    return image, raw_lidar, gt_lidar, pseudo_depth_map, pseudo_gt_map
+
 
 if __name__ == '__main__':
     datapath = ''
-    images, lidars, gt, pseudo, pseudo_gt = dataloader(datapath)
+    image, raw_lidar, gt_lidar, pseudo_depth_map, pseudo_gt_map = dataloader(datapath)
+
     print("Data loading complete.\n")
--- dataloader/trainLoader.py
+++ dataloader/trainLoader.py
@@ -1,44 +1,26 @@
 import os
 import torch.utils.data as data
 import random
-from PIL import ImageFile
-ImageFile.LOAD_TRUNCATED_IMAGES = True
 import skimage
 import skimage.io
 import numpy as np
 import torchvision.transforms as transforms
-
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-INSTICS = {"2011_09_26": [721.5377, 596.5593, 149.854],
-           "2011_09_28": [707.0493, 604.0814, 162.5066],
-           "2011_09_29": [718.3351, 600.3891, 159.5122],
-           "2011_09_30": [707.0912, 601.8873, 165.1104],
-           "2011_10_03": [718.856, 607.1928, 161.2157]
-}
-# INSTICS = {"NYU": [582.6245, 313.0448, 238.4439]}
-__imagenet_stats = {'mean': [0.485, 0.456, 0.406],
-                   'std': [0.229, 0.224, 0.225]}
-
-
-def scale_crop(input_size, scale_size=None, normalize=__imagenet_stats):
+def get_transform():
     t_list = [
         transforms.ToTensor(),
     ]
-
     return transforms.Compose(t_list)
 
-def get_transform(name='imagenet', input_size=None,
-                  scale_size=None, normalize=None, augment=True):
-    normalize = __imagenet_stats
-    input_size = 256
-    return scale_crop(input_size=input_size,
-                              scale_size=scale_size, normalize=normalize)
 
 def default_loader(path):
     img = skimage.io.imread(path)
     return img
+
 
 def point_loader(path):
     img = skimage.io.imread(path)
@@ -46,14 +28,16 @@ def point_loader(path):
     depth = np.reshape(depth, [img.shape[0], img.shape[1], 1]).astype(np.float32)
     return depth
 
+
 class myImageFloder(data.Dataset):
-    def __init__(self, raw_image,law_lidar,gt_lidar,pseudo_depth,gt_depth,training,
+    def __init__(self, raw_image, raw_lidar, gt_lidar, pseudo_depth_map, pseduo_gt_map, training,
                  loader=default_loader, ploader = point_loader):
         self.raw_image = raw_image
-        self.law_lidar = law_lidar
         self.gt_lidar = gt_lidar
-        self.pseudo_depth = pseudo_depth
-        self.gt_depth = gt_depth
+        self.raw_lidar = raw_lidar
+        self.pseudo_depth_map = pseudo_depth_map
+        self.pseduo_gt_map = pseduo_gt_map
+        
         self.loader = loader
         self.gtploader = ploader
         self.ploader = ploader 
@@ -62,48 +46,51 @@ class myImageFloder(data.Dataset):
     def __getitem__(self, index):
         raw_image = self.raw_image[index]
         gt_lidar = self.gt_lidar[index]
-        law_lidar = self.law_lidar[index]
-        pseudo_depth = self.pseudo_depth[index]
-        gt_depth = self.gt_depth[index]
+        raw_lidar = self.raw_lidar[index]
+        pseudo_depth_map = self.pseudo_depth_map[index]
+        pseduo_gt_map = self.pseduo_gt_map[index]
 
-        raw_image = self.loader(raw_image)  #raw image shape: (375, 1242, 3)
+        raw_image = self.loader(raw_image)
         h,w,c= raw_image.shape
 
-        # dataload & normalization
-        gt_point = self.gtploader(gt_lidar)
-        law_lidar = self.ploader(law_lidar)
-        pseudo_depth = self.ploader(pseudo_depth)
-        gt_depth = self.ploader(gt_depth)
-        
-        # Data loaded: gt_point (375, 1242, 1), law_lidar (375, 1242, 1), pseudo_depth (375, 1242, 1), gt_depth (375, 1242, 1)
+        # shape of raw_image: (375, 1242, 3)
 
-        #random crop
+        # data load & normalization
+        gt_lidar = self.gtploader(gt_lidar)
+        raw_lidar = self.ploader(raw_lidar)
+        pseudo_depth_map = self.ploader(pseudo_depth_map)
+        pseduo_gt_map = self.ploader(pseduo_gt_map)
+        
+        # shape of loaded data: gt_lidar (375, 1242, 1) | raw_lidar (375, 1242, 1) | pseudo_depth_map (375, 1242, 1) | pseduo_gt_map (375, 1242, 1)
+
+        # random crop
         th, tw = 256,512
         x1 = random.randint(0, w - tw)
         y1 = random.randint(0, h - th)
 
         raw_image = raw_image[y1:y1 + th, x1:x1 + tw, :]
-        gt_point = gt_point[y1:y1 + th, x1:x1 + tw,:]
-        law_lidar = law_lidar[y1:y1 + th, x1:x1 + tw, :]
-        pseudo_depth = pseudo_depth[y1:y1 + th, x1:x1 + tw, :]
-        gt_depth = gt_depth[y1:y1 + th, x1:x1 + tw, :]
-        processed = get_transform(augment=False)
+        gt_lidar = gt_lidar[y1:y1 + th, x1:x1 + tw,:]
+        raw_lidar = raw_lidar[y1:y1 + th, x1:x1 + tw, :]
+        pseudo_depth_map = pseudo_depth_map[y1:y1 + th, x1:x1 + tw, :]
+        pseduo_gt_map = pseduo_gt_map[y1:y1 + th, x1:x1 + tw, :]
+        processed = get_transform()
 
-        # Cropped data shapes: raw_image (256, 512, 3), gt_point (256, 512, 1), law_lidar (256, 512, 1), pseudo_depth (256, 512, 1), gt_depth (256, 512, 1)
+        # shape of cropped data: raw_image (256, 512, 3) | gt_lidar (256, 512, 1) | raw_lidar (256, 512, 1) | pseudo_depth_map (256, 512, 1) | pseduo_gt_map (256, 512, 1)
         
-        # ToTensor
+        # convert to tensor
         raw_image = processed(raw_image)
-        law_lidar = processed(law_lidar)
-        pseudo_depth = processed(pseudo_depth)
-        gt_depth = processed(gt_depth)
-        gt_point = processed(gt_point)
+        raw_lidar = processed(raw_lidar)
+        pseudo_depth_map = processed(pseudo_depth_map)
+        pseduo_gt_map = processed(pseduo_gt_map)
+        gt_lidar = processed(gt_lidar)
 
-        # Data transformed: raw_image torch.Size([3, 256, 512]), law_lidar torch.Size([1, 256, 512]), pseudo_depth torch.Size([1, 256, 512]), gt_depth torch.Size([1, 256, 512]), gt_point torch.Size([1, 256, 512])
+        # shape of transformed data: raw_image torch.Size([3, 256, 512]) | gt_lidar torch.Size([1, 256, 512]) | raw_lidar torch.Size([1, 256, 512]) | pseudo_depth_map torch.Size([1, 256, 512]) | pseduo_gt_map torch.Size([1, 256, 512])
 
-        return raw_image,gt_point,law_lidar,pseudo_depth,gt_depth
+        return raw_image, gt_lidar, raw_lidar, pseudo_depth_map, pseduo_gt_map
 
     def __len__(self):
         return len(self.raw_image)
+
 
 if __name__ == '__main__':
     print("")
