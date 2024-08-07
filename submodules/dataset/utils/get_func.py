8a5cf974c86ad435b75719cf11123559e972723d
9-coding <9.coding.lab@gmail.com>
2024-08-07 16:08:22 +0900

    REFACTOR: image_processing.py and add get_func.py for refactoring

---
+++ submodules/dataset/utils/get_func.py
@@ -0,0 +1,24 @@
+import os
+from tqdm import tqdm
+
+def get_sync_path(root_dir, kitti_raw_list):
+    for date in tqdm(kitti_raw_list):
+        date_path = os.path.join(root_dir, date)
+        if os.path.exists(date_path) and os.path.isdir(date_path):
+            for sync in os.listdir(date_path):
+                sync_path = os.path.join(date_path, sync)
+    return sync_path
+
+def get_last_5(dir_path):
+    last_5 = []
+    file_names = os.listdir(dir_path)
+    file_names.sort()
+    last_file_name = file_names[-1] if file_names else None
+    last_file_int = int(last_file_name[:-4])
+
+    for i in range(5):
+        file_name = last_file_int - i
+        file = str(file_name).zfill(10) + '.png'
+        last_5.append(file)
+
+    return last_5
--- submodules/dataset/utils/image_preprocessing.py
+++ submodules/dataset/utils/image_preprocessing.py
@@ -3,6 +3,8 @@ import shutil
 
 from tqdm import tqdm
 
+from submodules.dataset.utils.get_func import get_sync_path, get_last_5
+
 kitti_raw_list = ['kitti_raw/2011_09_26',
                   'kitti_raw/2011_09_28',
                   'kitti_raw/2011_09_29',
@@ -14,77 +16,43 @@ first_5 = ['0000000000.png', '0000000001.png', '0000000002.png', '0000000003.png
 
 # 사용하지 않는 grayscale image 제거
 def remove_unused_files(root_dir, kitti_raw_list):
-    for date in tqdm(kitti_raw_list):
-        date_path = os.path.join(root_dir, date)
-        if os.path.exists(date_path) and os.path.isdir(date_path):
-            for sync in os.listdir(date_path):
-                sync_path = os.path.join(date_path, sync)
-                if os.path.exists(sync_path) and os.path.isdir(sync_path):
-                    try:
-                        shutil.rmtree(os.path.join(sync_path, 'image_00'))
-                        shutil.rmtree(os.path.join(sync_path, 'image_01'))
-                        shutil.rmtree(os.path.join(sync_path, 'oxts'))
-                        shutil.rmtree(os.path.join(sync_path, 'velodyne_points'))
-                    except Exception as e:
-                        print(e)
-                        continue
-
+    sync_path = get_sync_path(root_dir, kitti_raw_list)
+    if os.path.exists(sync_path) and os.path.isdir(sync_path):
+        try:
+            shutil.rmtree(os.path.join(sync_path, 'image_00'))
+            shutil.rmtree(os.path.join(sync_path, 'image_01'))
+            shutil.rmtree(os.path.join(sync_path, 'velodyne_points'))
+        except Exception as e:
+            print(e)
 
 def remove_oxts(root_dir, kitti_raw_list):
-    for date in tqdm(kitti_raw_list):
-        date_path = os.path.join(root_dir, date)
-        if os.path.exists(date_path) and os.path.isdir(date_path):
-            for sync in os.listdir(date_path):
-                sync_path = os.path.join(date_path, sync)
-                if os.path.exists(sync_path) and os.path.isdir(sync_path):
-                    try:
+    sync_path = get_sync_path(root_dir, kitti_raw_list)
+    if os.path.exists(sync_path) and os.path.isdir(sync_path):
+        try:
+            shutil.rmtree(os.path.join(sync_path, 'oxts'))
+        except Exception as e:
+            print(e)
 
-                        shutil.rmtree(os.path.join(sync_path, 'oxts'))
-                    except Exception as e:
-                        print(e)
-                        continue
-
+def move_folder(root_dir, kitti, list, dst):
+    # root_dir | image or lidar folder name | train or val inner folder list ! destination directory
+    for folder in list:
+        source_folder_path = os.path.join(root_dir, kitti, folder)
+        if os.path.isdir(source_folder_path):
+            shutil.move(source_folder_path, dst)
 
 def split_train_val(root_dir, kitti_raw_list, train_list, image_train_dir, val_list, image_val_dir):
     for kitti_raw in tqdm(kitti_raw_list):
         # train_list에 해당하는 폴더를 train 디렉토리로 이동
-        for folder in train_list:
-            source_folder_path = os.path.join(root_dir, kitti_raw, folder)
-            if os.path.isdir(source_folder_path):
-                shutil.move(source_folder_path, image_train_dir)
+        move_folder(root_dir, kitti_raw, train_list, image_train_dir)
 
         # val_list에 해당하는 폴더를 val 디렉토리로 이동
-        for folder in val_list:
-            source_folder_path = os.path.join(root_dir, kitti_raw, folder)
-            if os.path.isdir(source_folder_path):
-                shutil.move(source_folder_path, image_val_dir)
+        move_folder(root_dir, kitti_raw, val_list, image_val_dir)
         shutil.rmtree(os.path.join(root_dir, kitti_raw))
 
     shutil.rmtree(os.path.join(image_train_dir, 'image_02'))
     shutil.rmtree(os.path.join(image_train_dir, 'image_03'))
     shutil.rmtree(os.path.join(image_val_dir, 'image_02'))
     shutil.rmtree(os.path.join(image_val_dir, 'image_03'))
-
-
-def get_last_file_name(directory):
-    file_names = os.listdir(directory)
-    file_names.sort()
-
-    return file_names[-1] if file_names else None
-
-
-def get_last_5(dir_path):
-    last_5 = []
-    last_file_name = get_last_file_name(dir_path)
-    last_file_int = int(last_file_name[:-4])
-
-    for i in range(5):
-        file_name = last_file_int - i
-        file = str(file_name).zfill(10) + '.png'
-        last_5.append(file)
-
-    return last_5
-
 
 # lidar data와 장면을 맞추기 위해 이미지의 처음/마지막 5개 파일 제거.
 def remove_first_last_5(root_dir, image_train_dir, image_val_dir):
@@ -109,7 +77,7 @@ def remove_first_last_5(root_dir, image_train_dir, image_val_dir):
                     # 파일이 있는 경우에만 처리
                     if files:
                         # 첫 5개 파일 삭제
-                        for file in first_5:  # 첫 5개 파일
+                        for file in first_5:
                             file_path = os.path.join(dir_path, file)
                             try:
                                 if os.path.isfile(file_path):
@@ -117,7 +85,7 @@ def remove_first_last_5(root_dir, image_train_dir, image_val_dir):
                             except Exception as e:
                                 print(e)
                         last_5 = get_last_5(dir_path)
-                        for file in last_5:  # 첫 5개 파일
+                        for file in last_5:
                             file_path = os.path.join(dir_path, file)
                             try:
                                 if os.path.isfile(file_path):
